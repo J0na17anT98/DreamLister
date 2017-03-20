0ac7d9fd671f0e2a10ff5d1d1de0073952e9c403
J0na17anT98 <pilotj@outlook.com>
2017-03-19 23:21:46 -0400

    DreamLister with core data tutorial app made by devslopes

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Jonathan Tsistinas on 3/19/17.
+//  Copyright © 2017 Jonathan Tsistinas. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var picture: UIView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
